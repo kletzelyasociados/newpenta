549c582e10b20245869c499ba4cbb1635aaf8268
Manuel Fabela <jose.manuel.vilchis@gmail.com>
2018-10-23 20:38:31 -0500

    Testing of MODEL of XML
    Testing of code of model definition

---
+++ xmltoinvoiceMX/models/models.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class xml(models.Model):
+    _name = 'xml'
+
+    #name = fields.Char()
+    #value = fields.Integer()
+    #value2 = fields.Float(compute="_value_pc", store=True)
+    #description = fields.Text()
+
+    #@api.depends('value')
+    #def _value_pc(self):
+    #    self.value2 = float(self.value) / 100
+
+    rfc_emisor = fields.Text(string="RFC Emisor")
+    nombre_emisor = fields.Text(string="Nombre Emisor")
+    rfc_receptor = fields.Text(string="RFC Receptor")
+    nombre_receptor = fields.Text(string="Nombre Receptor")
+    subtotal = fields.Float(string="Subtotal")
+    total = fields.Float(string="Total")
+
+
