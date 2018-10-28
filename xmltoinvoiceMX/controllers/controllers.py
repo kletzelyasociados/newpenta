4c32e02e26975f4d271d5b2f93b7bd9f8ad66e3d
Manuel Fabela <jose.fabela@kletzelyasociados.com>
2018-10-27 23:03:25 -0500

    coding: utf-8 Deleted

--- xmltoinvoiceMX/controllers/controllers.py
+++ xmltoinvoiceMX/controllers/controllers.py
@@ -1,4 +1,3 @@
- -*- coding: utf-8 -*-
 from odoo import http
 
 
@@ -20,21 +19,4 @@ class MyModule(http.Controller):
              'object': obj
          })
 
-# class MyModule(http.Controller):
-#     @http.route('/my_module/my_module/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
-
-#     @http.route('/my_module/my_module/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('my_module.listing', {
-#             'root': '/my_module/my_module',
-#             'objects': http.request.env['my_module.my_module'].search([]),
-#         })
-
-#     @http.route('/my_module/my_module/objects/<model("my_module.my_module"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('my_module.object', {
-#             'object': obj
-#         })
 
