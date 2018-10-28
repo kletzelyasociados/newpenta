299b1d65f00c185e325b2e61e993c7605610a5f7
Manuel Fabela <jose.fabela@kletzelyasociados.com>
2018-10-27 23:12:19 -0500

    Fixed module name & manifest

--- xmltoinvoiceMX/controllers/controllers.py
+++ xmltoinvoiceMX/controllers/controllers.py
@@ -2,20 +2,20 @@ from odoo import http
 
 
 class MyModule(http.Controller):
-     @http.route('/my_module/my_module/', auth='public')
+     @http.route('/newpenta/xmltoinvoiceMX/', auth='public')
      def index(self, **kw):
          return "Hello, world"
 
-     @http.route('/my_module/my_module/objects/', auth='public')
+     @http.route('/newpenta/xmltoinvoiceMX/objects/', auth='public')
      def list(self, **kw):
-         return http.request.render('my_module.listing', {
-             'root': '/my_module/my_module',
-             'objects': http.request.env['my_module.my_module'].search([]),
+         return http.request.render('xmllisting', {
+             'root': '/newpenta/xmltoinvoiceMX',
+             'objects': http.request.env['xml'].search([]),
          })
 
-     @http.route('/my_module/my_module/objects/<model("my_module.my_module"):obj>/', auth='public')
+     @http.route('/newpenta/xmltoinvoiceMX/objects/<model("xml"):obj>/', auth='public')
      def object(self, obj, **kw):
-         return http.request.render('my_module.object', {
+         return http.request.render('xml.object', {
              'object': obj
          })
 
